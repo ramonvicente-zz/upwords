8f63835fe65059dd59b849da4ddb3e8084aaf92f
Ramon Vicente <ramonvicentee@gmail.com>
2021-11-07 20:15:29 -0300

    initial commit

---
+++ src/main/java/com/ramonvicente/upwords/UpwordsApplication.java
@@ -0,0 +1,13 @@
+package com.ramonvicente.upwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpwordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpwordsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramonvicente/upwords/UpwordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramonvicente.upwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpwordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
