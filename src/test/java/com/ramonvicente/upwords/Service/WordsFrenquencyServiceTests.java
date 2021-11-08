5aeda260492c3602a07a60de9728188b2fadf2dd
Ramon Vicente <ramonvicentee@gmail.com>
2021-11-07 21:19:58 -0300

    wip: start service layer

---
+++ src/test/java/com/ramonvicente/upwords/Service/WordsFrenquencyServiceTests.java
@@ -0,0 +1,13 @@
+package com.ramonvicente.upwords.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsFrenquencyServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
