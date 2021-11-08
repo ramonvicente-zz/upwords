e72841f7b3d913dbd8bd1bd55245ad0f55de3486
Ramon Vicente <ramonvicentee@gmail.com>
2021-11-07 21:19:58 -0300

    wip: start service layer

---
+++ src/main/java/com/ramonvicente/upwords/Service/WordsFrequencyService.java
@@ -0,0 +1,14 @@
+package com.ramonvicente.upwords.Service;
+
+import com.ramonvicente.upwords.Model.Notepad;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WordsFrequencyService {
+
+  public long getFrequencyOfWords(Notepad notepad) {
+    return 1L;
+  }
+  
+}
